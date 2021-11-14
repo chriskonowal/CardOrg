ccbd769e0cc06f71ff8cb73390e79cef6c88cb89
Chris Konowal <chriskonowal@gmail.com>
2021-11-14 07:54:57 -0500

    File cleanup

--- SQL/drop card table.sql
+++
@@ -1,9 +0,0 @@
-USE [CardOrg]
-GO
-
-/****** Object:  Table [dbo].[Cards]    Script Date: 10/23/2021 12:56:55 PM ******/
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Cards]') AND type in (N'U'))
-DROP TABLE [dbo].[Cards]
-GO
-
-
